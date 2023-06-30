1fb9eaef48dd7f84949848925bcd3d4790ec992d
saminshakya <saminshakya27@gmail.com>
2023-06-30 12:50:08 +0545

    created helper function

--- Compositional_layout/ViewController.swift
+++ Compositional_layout/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     }
     
     private func createLayout() -> UICollectionViewCompositionalLayout {
+        
         //item
         
         let item = CompositionalLayout.createItem(width: .fractionalWidth(0.5), height: .fractionalHeight(1), spacing: 1)
@@ -28,14 +29,19 @@ class ViewController: UIViewController {
         let fullItem = CompositionalLayout.createItem(width: .fractionalWidth(1), height: .fractionalHeight(1), spacing: 1)
         
         let verticalGroup = CompositionalLayout.createGroup(alignment: .vertical, width: .fractionalWidth(0.5), height: .fractionalHeight(1), item: fullItem, count: 2)
+        
         //Group
+        
         let group = CompositionalLayout.createGroup(alignment: .horizontal, width: .fractionalWidth(1), height: .fractionalHeight(0.3), items:  [item, verticalGroup])
+        
         //Section
+        
         let section = NSCollectionLayoutSection(group: group)
+        
         //return
+        
         return UICollectionViewCompositionalLayout(section: section)
     }
-    
 }
 
 
